097f88abcced565a00a9b60fd0ef080857352532
Phương Mỹ Chi <phuongmychifc@24hgame.vn>
2020-03-14 13:08:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1400,56 +1400,56 @@
                             ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
                         ])
                     }, i("home")), Oe(me.a, {
-                        href: "/privacy"
+                        href: "/privacy.html"
                     }, Oe("a", {
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ])
                     }, i("privacy"))), Oe("a", {
                         href: "mailto:lovephuongmychi@gmail.com",
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ])
                     }, i("contact"))), Oe("ul", {
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ]) + " social"
                     }, Oe("li", {
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ])
                     }, Oe("a", {
                         href: "https://twitter.com/lovephuongmychi",
                         target: "_blank",
                         rel: "noopener noreferrer",
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ]) + " twitter"
                     }, Oe(we, {
                         width: 16,
                         height: 16
                     }))), Oe("li", {
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ])
                     }, Oe("a", {
                         href: "https://www.instagram.com/phuongmychi/",
                         target: "_blank",
                         rel: "noopener noreferrer",
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ]) + " phuongmychi"
                     }, Oe(xe, {
                         width: 16,
                         height: 16
                     })))), Oe("span", {
                         className: h.a.dynamic([
-                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]]
+                            ["1455347606", [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.medium]]
                         ]) + " copyright"
                     }, "Copyright \xa9", Se), Oe(h.a, {
                         id: "1455347606",
                         dynamic: [n.darkGray, o.size.small, n.gray, n.gray, n.darkGray, o.size.small]
-                    }, ["footer.__jsx-style-dynamic-selector{padding:0 20px 30px 20px;text-align:center;margin-top:10%}", ".navigation.__jsx-style-dynamic-selector{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;}", ".navigation.__jsx-style-dynamic-selector a.__jsx-style-dynamic-selector{color:".concat(n.darkGray, ";font-size:").concat(o.size.small, ";margin:0 5px;-webkit-text-decoration:none;text-decoration:none;text-transform:uppercase;-webkit-transition:all 0.2s ease;transition:all 0.2s ease;}"), ".navigation.__jsx-style-dynamic-selector a.__jsx-style-dynamic-selector:hover{color:".concat(n.gray, ";}"), ".social.__jsx-style-dynamic-selector{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;list-style-type:none;margin:30px 0 40px 0;padding:0;}", ".twitter.__jsx-style-dynamic-selector,.phuongmychi.__jsx-style-dynamic-selector{fill:".concat(n.gray, ";margin:0 10px;}"), ".copyright.__jsx-style-dynamic-selector{color:".concat(n.darkGray, ";font-size:13px").concat(o.size.small, ";}")]))
+                    }, ["footer.__jsx-style-dynamic-selector{padding:0 20px 30px 20px;text-align:center;margin-top:15%}", ".navigation.__jsx-style-dynamic-selector{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;}", ".navigation.__jsx-style-dynamic-selector a.__jsx-style-dynamic-selector{color:".concat(n.darkGray, ";font-size:").concat(o.size.small, ";margin:0 5px;-webkit-text-decoration:none;text-decoration:none;text-transform:uppercase;-webkit-transition:all 0.2s ease;transition:all 0.2s ease;}"), ".navigation.__jsx-style-dynamic-selector a.__jsx-style-dynamic-selector:hover{color:".concat(n.gray, ";}"), ".social.__jsx-style-dynamic-selector{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;list-style-type:none;margin:30px 0 40px 0;padding:0;}", ".twitter.__jsx-style-dynamic-selector,.phuongmychi.__jsx-style-dynamic-selector{fill:".concat(n.gray, ";margin:0 10px;}"), ".copyright.__jsx-style-dynamic-selector{color:".concat(n.darkGray, ";font-size:12px").concat(o.size.small, ";}")]))
                 },
                 Ee = Object(O.b)()((function(e) {
                     return Oe(S.a.Consumer, null, (function(t) {
